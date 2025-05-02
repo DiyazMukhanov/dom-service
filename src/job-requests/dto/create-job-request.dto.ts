87e9ee7b5d12888c55ec993f603d3f80d0a0352c
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2025-05-02 07:18:47 +0400

    Removed requests validation

--- src/job-requests/dto/create-job-request.dto.ts
+++ src/job-requests/dto/create-job-request.dto.ts
@@ -1,29 +1,21 @@
-// src/job-requests/dto/create-job-request.dto.ts
-
-import { IsString, IsEmail, IsNotEmpty } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class CreateJobRequestDto {
   @IsString()
-  @IsNotEmpty({ message: 'Тип работы обязателен' })
   workType: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Описание работы обязательно' })
   workDescription: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Дата работы обязательна' })
   workDate: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Время работы обязательно' })
   workTime: string;
 
-  @IsEmail({}, { message: 'Некорректный email' })
-  @IsNotEmpty({ message: 'Email обязателен' })
+  @IsString()
   email: string;
 
   @IsString()
-  @IsNotEmpty({ message: 'Номер телефона обязателен' })
   phoneNumber: string;
 }
