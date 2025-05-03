0754cc4f8124c6917f92a6f4a24db2b901cfb816
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2025-05-03 14:50:25 +0400

    Updated data source

--- data-source.ts
+++ data-source.ts
@@ -3,14 +3,20 @@ import * as dotenv from 'dotenv';
 
 dotenv.config();
 
+const isProd = process.env.NODE_ENV === 'production';
+
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DATABASE_HOST,
-  port: Number(process.env.DATABASE_PORT),
-  username: process.env.DATABASE_USERNAME,
-  password: process.env.DATABASE_PASSWORD,
-  database: process.env.DATABASE_NAME,
-  entities: ['src/**/*.entity.ts'],
-  migrations: ['src/migrations/*.ts'],
+  ...(isProd
+    ? { url: process.env.DATABASE_URL }
+    : {
+        host: process.env.DATABASE_HOST,
+        port: Number(process.env.DATABASE_PORT),
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+      }),
+  entities: [isProd ? 'dist/**/*.entity.js' : 'src/**/*.entity.ts'],
+  migrations: [isProd ? 'dist/src/migrations/*.js' : 'src/migrations/*.ts'],
   synchronize: false,
 });
