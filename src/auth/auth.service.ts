c9977b9b19b68c5bcc283d0960e272f199bd023a
DIYAZ MUKHANOV <diyaz.mukhanov@gmail.com>
2025-05-03 14:36:29 +0400

    Replaced bycrypt with bycriptjs

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -7,7 +7,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Admin } from '../admins/entities/admin.entity';
 import { JwtService } from '@nestjs/jwt';
-import * as bcrypt from 'bcrypt';
+import * as bcrypt from 'bcryptjs';
 import { User } from '../users/user.entity';
 
 @Injectable()
